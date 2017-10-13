3643d54a24a5cc2cf54b3cfe67936027e7bc21e9
jcompagni10 <jcompagni@gmail.com>
2017-10-13 09:28:43 -0700

    fix .gits

---
+++ w1d5/trees/KnightPathFinder.rb
@@ -0,0 +1,63 @@
+require_relative "lib/00_tree_node.rb"
+
+class KnightPathFinder
+  attr_reader :move_tree
+
+  def initialize(pos)
+    @visited_positions = [pos]
+    @move_tree= build_move_tree
+  end
+
+  def valid_moves(pos)
+    all_moves = [[1, 2], [-1, 2], [1, -2], [-1, -2], [2, 1], [-2, 1],
+                  [2, -1], [-2, -1]]
+    squares = all_moves.map { |move| [(pos[0] + move[0]), (pos[1] + move[1])] }
+    squares.select {|square| on_board?(square)}
+  end
+
+  def on_board?(pos)
+    (0..7).include?(pos[0]) && (0..7).include?(pos[1])
+  end
+
+  def new_move_positions(pos)
+   new_positions = valid_moves(pos).select {|move| !@visited_positions.include?(move)}
+   @visited_positions.concat(new_positions)
+   new_positions
+  end
+
+  def build_move_tree
+    root = PolyTreeNode.new(@visited_positions.first)
+    queue = [root]
+    until queue.empty?
+      current_node = queue.shift
+      possible_moves = new_move_positions(current_node.value)
+      possible_moves.each do |move|
+        new_node = PolyTreeNode.new(move)
+        queue.push(new_node)
+        current_node.add_child(new_node)
+      end
+    end
+    root
+  end
+
+  def find_path(pos)
+    possible_ends = @move_tree.bfs_all(pos)
+    possible_ends.map! { |leaf| trace_path_back(leaf)}
+    possible_ends
+
+  end
+
+  def trace_path_back(end_pos)
+    path = [end_pos]
+    until path.first.parent.nil?
+      path.unshift(path.first.parent)
+    end
+    path.map(&:value)
+
+  end
+
+end
+
+k = KnightPathFinder.new([0,0])
+k.move_tree.children.length
+p k.find_path([7,7])
---
+++ w1d5/trees/lib/00_tree_node.rb
@@ -0,0 +1,62 @@
+class PolyTreeNode
+  attr_reader :value, :parent
+  attr_accessor :children
+
+  def initialize(value, children = [])
+    @value = value
+    @children = children
+    @parent = nil
+  end
+
+  def parent=(parent)
+    return if parent == @parent
+    @parent.children.delete(self) unless @parent.nil?
+    @parent = parent
+    @parent.children << self unless parent.nil?
+    self
+  end
+
+  def add_child(child)
+    child.parent= (self)
+  end
+
+  def remove_child(child)
+    if @children.include?(child)
+      child.parent = (nil)
+    else
+      raise "this is not your baby"
+    end
+  end
+
+  def dfs(target_value)
+    return self if @value == target_value
+    @children.each do |child|
+      result = child.dfs(target_value)
+      return result if result
+    end
+    nil
+  end
+
+
+  def bfs(target_value)
+    queue = [self]
+    until queue.empty?
+      current_node = queue.shift
+      return current_node if current_node.value == target_value
+      queue.concat(current_node.children)
+    end
+    nil
+  end
+
+  def bfs_all(target_value)
+    all_values = []
+    queue = [self]
+    until queue.empty?
+      current_node = queue.shift
+      all_values << current_node if current_node.value == target_value
+      queue.concat(current_node.children)
+    end
+    all_values
+  end
+
+end
